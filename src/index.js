7e11673b9cab6cee89de8b31c01cbe7123bf2cfd
Alfiya <alfiya96960@gmail.com>
2023-01-30 21:32:43 +0530

    code add

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-const express=require('express')
-const app=express()
-const route=require('./route/route')
-const mongoose=require('mongoose')
-var cors = require('cors')
-app.use(express.json())
-mongoose.connect("mongodb+srv://Vishanksingh:7997@cluster0.ga4iiwd.mongodb.net/Ankush1234-DB?retryWrites=true&w=majority",{dbName:"group4Database"},{useNewUrlParser:true}).then(()=>{console.log("mongoDb connected")}).catch((error)=>{console.log(error.message)})
+const express = require("express");
+const app = express();
+const route = require("./route/route");
+const cors = require("cors");
+const mongoose = require("mongoose");
+var cors = require("cors");
+app.use(express.json());
+mongoose
+    .connect(
+        "mongodb+srv://Vishanksingh:7997@cluster0.ga4iiwd.mongodb.net/Ankush1234-DB?retryWrites=true&w=majority",
+        { dbName: "group4Database" },
+        { useNewUrlParser: true }
+    )
+    .then(() => {
+        console.log("mongoDb connected");
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
 
-app.use('/',route)
+app.use("/", route);
 
-app.listen(3000,()=>{
-    console.log("app is running on port 3000")
-})
+app.listen(3000, () => {
+    console.log("app is running on port 3000");
+});
 
-app.use(cors())
+app.use(cors());
 
 app.use((err, req, res, next) => {
     if (err.message === "Unexpected end of JSON input") {
-      return res.status(400).send({status: false, message: "ERROR Parsing Data, Please Provide a Valid JSON",});
+        return res
+            .status(400)
+            .send({
+                status: false,
+                message: "ERROR Parsing Data, Please Provide a Valid JSON",
+            });
     } else {
-      next()
+        next();
     }
-  })
+});
